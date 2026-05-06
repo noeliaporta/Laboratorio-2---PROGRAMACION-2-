82780834a6fa89d7f0cdf83223e51cd6cc305abd
noeliaporta <portanooeliabri@gmail.com>
2026-05-06 11:17:04 -0300

    Agregue los getters y setters, tambien las funciones a desarrollar.

--- Empresa_MEG/src/main/java/org/example/Auto.java
+++ Empresa_MEG/src/main/java/org/example/Auto.java
@@ -1,4 +1,45 @@
 package Empresa_MEG.src.main.java.org.example;
 
 public class Auto {
+    String marca;
+    String modelo;
+    String color;
+    Motor motor;
+    double precio;
+
+    public Auto(Motor motor) {
+        this.motor = motor;
+    }
+
+    public Motor getMotor() {
+        return motor;
+    }
+
+    public void setMotor(Motor motor) {
+        this.motor = motor;
+    }
+
+    @Override
+    public void avanzar(int km){
+
+    }
+
+    @Override
+    public boolean requiereCambioAceite(){
+
+    }
 }
+/*
+Clase Auto
+Atributos:
+marca
+modelo
+color
+Motor motor (composición)
+precio
+Métodos:
+avanzar(int km):
+Incrementa los kilómetros recorridos del motor.
+Invoca el método requiereCambioAceite() del motor.
+Si devuelve true, mostrar un mensaje con JOptionPane.showMessageDialog indicando que el auto necesita cambio de aceite.
+ */
