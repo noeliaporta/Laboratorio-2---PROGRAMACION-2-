b41edf4e840f9c3bda3743fb62923f9166951368
Lucas <lucasbubans8@gmail.com>
2026-05-06 10:20:46 -0300

    Fast_Master

--- Empresa_MEG/src/main/java/org/example/Motor.java
+++ Empresa_MEG/src/main/java/org/example/Motor.java
@@ -1,4 +1,16 @@
 package Empresa_MEG.src.main.java.org.example;
 
 public class Motor {
+    double kilometrosRecorridos;
+    float cilindrada; //(por ejemplo: 1.6, 2.0, etc.)
+    int caballosFuerza;
+
+
+
+    public boolean requiereCambioAceite(double kilometrosRecorridos){
+        boolean temp = false;
+        if (kilometrosRecorridos >= 100000)
+            temp = true;
+        return temp;
+    }
 }
